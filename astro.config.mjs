e714eae3a2229a3581519898741c076ac5d222f5
JoseDv1 <jose123millon@gmail.com>
2024-07-03 20:25:18 -0500

    chore: Update Astro config for static output and Vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,10 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import svelte from "@astrojs/svelte";
-
 import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [svelte()],
-  output: "server",
-  adapter: vercel()
+	integrations: [svelte()],
+	output: "static",
+	adapter: vercel(),
 });
