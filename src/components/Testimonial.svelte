01014de2b4c9f1e604e789eb67ad9d8dd7c8f78e
JoseDv1 <jose123millon@gmail.com>
2024-08-14 15:39:04 -0500

    Update: Change retreats layout

---
+++ src/components/Testimonial.svelte
@@ -0,0 +1,28 @@
+<script>
+	export let text;
+	export let name;
+</script>
+
+<section>
+	<p>{text}</p>
+	<b>{name}</b>
+</section>
+
+<style>
+	section {
+		margin-block: var(--section-gap);
+		background-color: var(--fg-color);
+		padding: 2rem;
+		border-radius: var(--border-radius);
+	}
+
+	p {
+		color: var(--accent-color);
+	}
+
+	b {
+		display: block;
+		margin-top: 1rem;
+		color: var(--bg-color);
+	}
+</style>
